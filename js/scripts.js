c58dc97b52a1f631f6e8ea49fbefda2ceaf05626
Adam Ross Russell and Jon Sweeney <adamrr724@yahoo.com>
2016-01-21 15:28:13 -0800

    add two ways of doing roman numeral function

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,112 @@
-var countUp = function(countTo, countBy) {
-  if (countTo <= 0 || countBy <= 0) {
-    return "Please enter a positive number";
-  } else if (countBy <= countTo) {
-    return multiplyUp(countTo, countBy);
-  } else {
-    return "Invalid Input";
+var roman = function(num) {
+  var romanNum = [];
+  while (num >= 1000) {
+    romanNum.push("M");
+    num -= 1000;
   }
+  romanNum = romanNum.concat(craziness(num));
+
+  return romanNum.join("");
 };
 
-var multiplyUp = function(countTo, countBy) {
-  var multiples = [];
+var craziness = function(num) {
+  var arrayOfArrays = [[500, 900, "CM", "D"],
+                       [100, 400, "CD", "C"],
+                       [50, 90, "XC", "L"],
+                       [10, 40, "XL", "X"],
+                       [5, 9, "IX", "V"],
+                       [1, 4, "IV", "I"]];
+  var romanNums = [];
 
-  for (var i = countBy; i <= countTo; i += countBy) {
-    multiples.push(i);
+  for(var i = 0; i < arrayOfArrays.length; i++){
+    var romanArray = arrayOfArrays[i];
+    var x = romanArray[0];
+    var y = romanArray[1];
+    var yy = romanArray[2];
+    var xx = romanArray[3];
+
+    while(num >= x) {
+      if(num >= y) {
+        romanNums.push(yy);
+        num -= y;
+      } else {
+        romanNums.push(xx);
+        num -= x;
+      }
+    }
   }
-  return multiples;
-};
+  return romanNums;
+}
 
+  // while(num >= 500) {
+  //   if(num >= 900) {
+  //     romanNum.push("CM");
+  //     num -= 900;
+  //   } else {
+  //     romanNum.push("D");
+  //     num -= 500;
+  //   }
+  // }
+  //
+  // while(num >= 100) {
+  //   if(num >= 400){
+  //     romanNum.push("CD");
+  //     num -= 400;
+  //   } else {
+  //     romanNum.push("C");
+  //     num -= 100;
+  //   }
+  // }
+  //
+  // while(num >= 50) {
+  //   if(num >= 90) {
+  //     romanNum.push("XC");
+  //     num -= 90;
+  //   } else {
+  //     romanNum.push("L");
+  //     num -= 50;
+  //   }
+  // }
+  //
+  // while(num >= 10) {
+  //   if(num >= 40) {
+  //     romanNum.push("XL");
+  //     num -= 40;
+  //   } else {
+  //     romanNum.push("X");
+  //     num -= 10;
+  //   }
+  // }
+  //
+  // while(num >= 5) {
+  //   if(num >= 9) {
+  //     romanNum.push("IX");
+  //     num -= 9;
+  //   } else {
+  //     romanNum.push("V");
+  //     num -= 5;
+  //   }
+  // }
+  //
+  // while(num >= 1) {
+  //   if(num >= 4) {
+  //     romanNum.push("IV");
+  //     num -= 4;
+  //   } else {
+  //     romanNum.push("I");
+  //     num -= 1;
+  //   }
+  // }
+
+
+//};
 
 
 $(document).ready(function(event) {
-    $("form#count").submit(function(event){
+    $("form#roman").submit(function(event){
 
-      var countBy= parseInt($("input#message1").val());
-      var countTo = parseInt($("input#message2").val());
-      var result = countUp(countTo, countBy);
+      var input = parseInt($("input#message1").val());
+      var result = roman(input);
 
   $(".result").text(result);
   $("#result").show();
--- js/specs.js
+++ js/specs.js
@@ -1,27 +1,55 @@
-describe("countUp", function() {
+describe("roman", function() {
 
-  it("returns false if countTo is less than countBy", function() {
-    expect(countUp(2, 3)).to.equal("Invalid Input");
+  it("will return M for 1000", function() {
+    expect(roman(2000)).to.eql("MM");
   });
 
-  it ("returns false if either input is a negative number", function() {
-    expect(countUp(-3, -4)).to.equal("Please enter a positive number");
-  });
-});
-
-
-describe("multiplyUp", function() {
-
-  it("returns array if countTo is less than countBy", function() {
-    expect(multiplyUp(3, 3)).to.eql([3]);
+  it("will return MMD for 2500", function() {
+    expect(roman(2500)).to.eql("MMD");
   });
 
-  it("returns array if countTo is greater than countBy", function() {
-    expect(multiplyUp(5, 3)).to.eql([3]);
+  it("will return CC for 200", function() {
+    expect(roman(200)).to.eql("CC");
   });
 
-  it("returns array if with multiples up to countTo", function() {
-    expect(multiplyUp(15, 5)).to.eql([5, 10, 15]);
+  it("will return L for 50", function() {
+    expect(roman(50)).to.eql("L");
+  });
+
+  it("will return X for 10", function() {
+    expect(roman(10)).to.eql("X");
+  });
+
+  it("will return V for 5", function() {
+    expect(roman(5)).to.eql("V");
+  });
+
+  it("will return I for 1", function() {
+    expect(roman(1)).to.eql("I");
+  });
+
+  it("will return MCM for 1900", function() {
+    expect(roman(1900)).to.eql("MCM");
+  });
+
+  it("will return CD for 400", function() {
+    expect(roman(400)).to.eql("CD");
+  });
+
+  it("will return XC for 90", function() {
+    expect(roman(90)).to.eql("XC");
+  });
+
+  it("will return XL for 40", function() {
+    expect(roman(40)).to.eql("XL");
+  });
+
+  it("will return IX for 9", function() {
+    expect(roman(9)).to.eql("IX");
+  });
+
+  it("will return IV for 4", function() {
+    expect(roman(4)).to.eql("IV");
   });
 
 });
